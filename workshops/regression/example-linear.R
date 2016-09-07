0b371c93b54f35555359f2bd8906230a7a751d1d
Andy Choens <andy.choens@gmail.com>
2016-09-07 16:17:17 -0400

    Addresses a lot of #17.
    - Sets up a new folder for the reression material,
    - Introduces a new set of slides for linear regression, based on
    - Anscombe's Quartet.
    - Introduces a revised .R script to accompany the discussion.
    
     Changes to be committed:
    	new file:	public/anscombe_quartet_3png
    	new file:	public/img/anscombe_quartet_3.png
    	new file:	public/img/anscombes_quartet_3.svg
    	new file:	public/img/francis_anscombe.jpeg
    	new file:	public/img/model-anscombe-1.png
    	new file:	public/img/qq-anscombe-1.png
    	new file:	public/img/scatter-anscombe-1.png
    	deleted:	workshops/logistic-crash/.Rhistory
    	deleted:	workshops/logistic-crash/example-linear.R
    	deleted:	workshops/logistic-crash/example-linear.html
    	deleted:	workshops/logistic-crash/notes.txt
    	renamed:	workshops/logistic-crash/about.html -> workshops/regression/about.html
    	renamed:	workshops/logistic-crash/data-dictionary.md -> workshops/regression/data-dictionary.md
    	modified:	workshops/regression/example-linear.R
    	new file:	workshops/regression/example-linear.html
    	renamed:	workshops/logistic-crash/first-plots.R -> workshops/regression/first-plots.R
    	renamed:	workshops/logistic-crash/first-predictive-model.R -> workshops/regression/first-predictive-model.R
    	renamed:	workshops/logistic-crash/first-predictive.html -> workshops/regression/first-predictive.html
    	renamed:	workshops/logistic-crash/import-explore.R -> workshops/regression/import-explore.R
    	renamed:	workshops/logistic-crash/import-explore.html -> workshops/regression/import-explore.html
    	renamed:	workshops/logistic-crash/index.html -> workshops/regression/index.html
    	renamed:	workshops/logistic-crash/kaggle.html -> workshops/regression/kaggle.html
    	renamed:	workshops/logistic-crash/logistic-regression-age.R -> workshops/regression/logistic-regression-age.R
    	renamed:	workshops/logistic-crash/logistic-regression-age.html -> workshops/regression/logistic-regression-age.html
    	renamed:	workshops/logistic-crash/logistic-regression-sex.R -> workshops/regression/logistic-regression-sex.R
    	renamed:	workshops/logistic-crash/logistic-regression-sex.html -> workshops/regression/logistic-regression-sex.html
    	renamed:	workshops/logistic-crash/participant-instructions.md -> workshops/regression/participant-instructions.md

--- workshops/regression/example-linear.R
+++ workshops/regression/example-linear.R
@@ -5,81 +5,127 @@
 ##
 ## #############################################################################
 
-## Question: R you familiar with Anscombe's Quartet?
 
-## =============================================================================
-## Load Data
-## =============================================================================
 
+## Load Data -------------------------------------------------------------------
+## The Anscombe Quartet is built into R
 data(anscombe)
-
-## Use View(anscombe) to see the data out of the terminal.
 anscombe
 
+## To see other data available in R / R Packages on your machine:
+data()
 
-## =============================================================================
-## Descriptive Statistics
-## =============================================================================
 
-## It is easy to get the average for each column.
-## Note: The average is essentially the same for all of these.
+
+## Many Ways To View An Object -------------------------------------------------
+attributes(anscombe)
+
+summary(anscombe)
+
+str(anscombe)
+
+View(anscombe)
+
+head(anscombe)
+
+tail(anscombe)
+
+
+
+## Descriptive Statistics ------------------------------------------------------
+## It is easy to get the average for each column
+## Note: The average is (essentially) identical for each column
 colMeans(anscombe)
 
-## It is less easy to get the standard deviation.
-## Note: The standard deviation is basically the same for each of these.
+## It is less easy to get the standard deviation
+## Note: The standard deviation is also (essentially) identical
 cbind(x1 = sd(anscombe$x1),
       x2 = sd(anscombe$x2),
       x3 = sd(anscombe$x3),
       x4 = sd(anscombe$x4)
       )
 
-## Are the sets correlated with each other? (Hint: Yes)
-cor.test(anscombe$x1, anscombe$y1)
-cor.test(anscombe$x2, anscombe$y2)
-cor.test(anscombe$x3, anscombe$y3)
 
-## =============================================================================
-## Linear Regression (Set 1)
-## =============================================================================
 
-## Note the difference between this and cor.test!
-m1 <- lm(anscombe$y1~anscombe$x1)
+## Correlation -----------------------------------------------------------------
+## To test for correlation, we just have to declare x and y from a single set.
+## You don't have to label the inputs "x =" and "y =", R assumes you will do
+## this. I'm putting them in here explicitly for demonstrative purposes.
+cor.test(x = anscombe$x1, y = anscombe$y1)
 
+
+
+## Visualize -------------------------------------------------------------------
+plot(anscombe$x1, anscombe$y1, main="Anscome: Set 1", xlab="x1", ylab="y1")
+
+
+
+## Your Turn! ------------------------------------------------------------------
+## Are the correlations of Sets 2 - 4 the same?
+cor.test()
+
+## What can you learn from:
+?cor.test
+
+## Can you reproduce the other scatterplots from Wikipedia?
+plot()
+
+
+
+## Linear Regression (Set 1) ---------------------------------------------------
+## We don't have a x var and a y var any more. We have a var called formula.
+## cor.test can have only two inputs. A linear regression can have many.
+## R uses the formula syntax y~x (y as explained by x) to handle this
+## complexity.
+m1 <- lm(formula=anscombe$y1~anscombe$x1)
 m1
 
-summary(m1)
+## See ?formula to learn more about the R formula syntax for multiple regression.
+
+
+## Many Ways To View An Object -------------------------------------------------
+## This time, we can see different ways to see a model.
 
 attributes(m1)
 
-plot(anscombe$x1, anscombe$y1)
-abline(m1, col="red")
-lines(lowess(anscombe$x1, anscombe$y1), col="blue")
+summary(m1)
 
+str(m1)
+
+head(m1)
+
+tail(m1)
+
+
+
+## Visualize! ------------------------------------------------------------------
+## Same scatterplot as before. This adds the linear model, m1.
+plot(anscombe$x1, anscombe$y1, main="Anscome: Set 1 w/ Model in Red", xlab="x1", ylab="y1")
+abline(m1, col="red")
+
+
+
+## Evaluating The Model - QQ Plot ----------------------------------------------
+## It is easy to get a standard QQ Plot of the model.
 qqplot(anscombe$x1,anscombe$y1)
 abline(m1, col="red")
 
 
-## =============================================================================
-## Linear Regression (Set 2)
-## =============================================================================
 
+## Your Turn! ------------------------------------------------------------------
+## But R can do more than just a QQ plot to explore a model.
+plot(m1)
 
-## What do you see?
-
-
-
-## =============================================================================
-## Linear Regression (Set 3)
-## =============================================================================
-
-
-## What do you see?
+## If you made other models, take a moment to explore and compare them now.
+plot()
 
 
 
-## =============================================================================
-## Linear Regression (Set 4)
-## =============================================================================
+## Exporting Graphics ----------------------------------------------------------
+## Exports the QQ Plot grapic shown previously for Set 1.
+## Exports the file as a PNG file, in the CWD.
+png("qq-anscombe-1.png")
+qqplot(anscombe$x1, anscombe$y1, main="Anscome: Set 1 w/ Model in Red", xlab="x1", ylab="y1")
+abline(m1, col="red")
+dev.off()
 
-
-## What do you see?
